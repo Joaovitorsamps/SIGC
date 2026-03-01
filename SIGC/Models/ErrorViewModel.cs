e88a72fcb23f4881ac57affde659b8f44f5055f2
JoaoSampaio-Jabil <joao_sampaio@jabil.com>
2026-02-28 23:25:46 -0400

    Primeiro commmit

---
+++ SIGC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIGC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
